074df67125d9205a778db79b76bd3f0c1a64b2e8
MiChang <immi5.tw@gmail.com>
2015-12-01 13:28:39 +0800

    social share fixed / crazy ad / links / card collect /

--- js/main.js
+++ js/main.js
@@ -47,16 +47,9 @@ $(function(){
 			}
 		});
 	});
-	$('.modal').on('hidden.bs.modal', function () {
-		$('video').get(0).pause();
-	})
-	$('.modal').on('hidden.bs.modal', function () {
-		$('audio').get(0).pause();
-	})
-	$('#portfolio_cindy-2').on('hidden.bs.modal', function () {
-		$('#portfolio_cindy-2 audio').get(0).pause();
-	})
-	
+	$(".modal#crazyad-video").on('hidden.bs.modal', function () {
+		$("#crazyad-video iframe").attr("src", $("#crazyad-video iframe").attr("src"));
+	});
 })(jQuery);
 
 $(function(){  
@@ -73,63 +66,6 @@ $(function(){
 		});
 	}
 );
-// external js: isotope.pkgd.js
-
-//$( document ).ready( function() {
-//  // init Isotope
-//  var $container = $('.isotope').isotope({
-//    itemSelector: '.element-item',
-//    layoutMode: 'fitRows',
-//    getSortData: {
-//      name: '.name',
-//      symbol: '.symbol',
-//      number: '.number parseInt',
-//      category: '[data-category]',
-//      weight: function( itemElem ) {
-//        var weight = $( itemElem ).find('.weight').text();
-//        return parseFloat( weight.replace( /[\(\)]/g, '') );
-//      }
-//    }
-//  });
-//
-//  // filter functions
-//  var filterFns = {
-//    // show if number is greater than 50
-//    numberGreaterThan50: function() {
-//      var number = $(this).find('.number').text();
-//      return parseInt( number, 10 ) > 50;
-//    },
-//    // show if name ends with -ium
-//    ium: function() {
-//      var name = $(this).find('.name').text();
-//      return name.match( /ium$/ );
-//    }
-//  };
-//
-//  // bind filter button click
-//  $('#filters').on( 'click', 'button', function() {
-//    var filterValue = $( this ).attr('data-filter');
-//    // use filterFn if matches value
-//    filterValue = filterFns[ filterValue ] || filterValue;
-//    $container.isotope({ filter: filterValue });
-//  });
-//
-//  // bind sort button click
-//  $('#sorts').on( 'click', 'button', function() {
-//    var sortByValue = $(this).attr('data-sort-by');
-//    $container.isotope({ sortBy: sortByValue });
-//  });
-//  
-//  // change is-checked class on buttons
-//  $('.button-group').each( function( i, buttonGroup ) {
-//    var $buttonGroup = $( buttonGroup );
-//    $buttonGroup.on( 'click', 'button', function() {
-//      $buttonGroup.find('.is-checked').removeClass('is-checked');
-//      $( this ).addClass('is-checked');
-//    });
-//  });
-//  
-//});
 
 $( document ).ready( function() {
   // init Isotope
@@ -184,5 +120,4 @@ $( document ).ready( function() {
       $( this ).addClass('is-checked');
     });
   });
-  
 });
